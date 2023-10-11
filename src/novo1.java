c9460242eaee97d955c1cee8bc84b9e183855ef2
matheus silva <matheusmmjm989@gmail.com>
2023-10-11 15:25:26 -0300

    "atividades do curso de java"

---
+++ src/novo1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class novo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1,s2;
+        int c;
+
+        c = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+
+        System.out.println(c);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
