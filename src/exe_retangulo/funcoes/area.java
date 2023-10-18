526aa0826abc2c73ea77c9d8afb3898388f206b1
matheus silva <matheusmmjm989@gmail.com>
2023-10-18 16:27:03 -0300

    "exercicio para treinar classe (retangulo)"

---
+++ src/exe_retangulo/funcoes/area.java
@@ -0,0 +1,16 @@
+package exe_retangulo.funcoes;
+
+public class area {
+
+    public double length;
+    public double width;
+
+    public double valueArea() {
+        return width * length;
+    }
+
+    public double perimeter() {
+        return (width + length)*2;
+    }
+
+}
