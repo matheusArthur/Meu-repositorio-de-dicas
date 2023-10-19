a539ff61a1c8db63fbf12864d0741e9d6801c8c3
matheus silva <matheusmmjm989@gmail.com>
2023-10-19 18:36:11 -0300

    "exercicio para treinar classe (salary)"

---
+++ src/exe_salary/app/program.java
@@ -0,0 +1,31 @@
+package exe_salary.app;
+
+import exe_salary.employee.salary;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        salary salaryy = new salary();
+
+        System.out.print("Enter your gross salary: ");
+        salaryy.grossSalary = sc.nextDouble();
+
+        System.out.print("Enter your tax: ");
+        salaryy.tax = sc.nextDouble();
+
+        System.out.println("Your net salary is: "+ salaryy.netSalary());
+
+        System.out.println("Increase salary: ");
+        double grossSalary = sc.nextDouble();
+        salaryy.addSalary(grossSalary);
+
+        System.out.println("your now salary is: "+ salaryy.grossSalary);
+
+    }
+}
---
+++ src/exe_salary/employee/salary.java
@@ -0,0 +1,17 @@
+package exe_salary.employee;
+
+public class salary {
+
+    public String name;
+    public double grossSalary;
+    public double addSalary;
+    public double tax;
+
+    public double netSalary() {
+        return grossSalary -= tax ;
+    }
+
+    public void addSalary(double grossSalary) {
+        this.grossSalary += grossSalary;
+    }
+}
