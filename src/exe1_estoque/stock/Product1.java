f295520812db0d8e3d7c5523b38276697061bdb0
matheus silva <matheusmmjm989@gmail.com>
2023-10-24 17:03:10 -0300

    "Obrigando o utilizador a preencher o nome, quantidade e preço de x produto pois não faz sentido criar um produto sem preço,nome ou quantidade"

--- src/exe1_estoque/stock/Product1.java
+++ src/exe1_estoque/stock/Product1.java
@@ -6,9 +6,13 @@ public class Product1 {
     public double price;
     public int quantity;
 
-    public double totalValueInStuck (){
-
-        return price*quantity  ;
+    public Product1(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public double totalValueInStuck() {
+        return quantity * price;
     }
     public void addProduct (int quantity){
 
