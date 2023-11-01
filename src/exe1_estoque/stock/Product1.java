43d6c13fbfcb8d2b1f81d7d7004d78829b48aeba
matheus silva <matheusmmjm989@gmail.com>
2023-11-01 16:17:20 -0300

    "Exercicio simples de conta bancaria-> saque e depositar"

--- src/exe1_estoque/stock/Product1.java
+++ src/exe1_estoque/stock/Product1.java
@@ -6,7 +6,7 @@ public class Product1 {
     public double price;
     public int quantity;
 
-    public Product1(String name, double price, int quantity) {
+    public Product1 (String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -15,16 +15,14 @@ public class Product1 {
         return quantity * price;
     }
     public void addProduct (int quantity){
-
         this.quantity += quantity;
     }
     public void removeProduct (int quantity){
-
         this.quantity -= quantity;
     }
 
     public String toSting(){
         return name + ", $ "+String.format("%.2f", price)+" , "+quantity+" units, Total : $" + String.format("%.2f", totalValueInStuck());
-
     }
+
 }
